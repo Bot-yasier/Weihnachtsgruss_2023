f02ed711a8642f2f76115e0aac6e17fc4c663550
Bot-yasier <89403943+Bot-yasier@users.noreply.github.com>
2023-04-24 14:38:24 +0200

    V2

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -1,56 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f; // Die Geschwindigkeit des Charakters
-    public float stopDistance = 0.1f; // Der Schwellenwert, bei dem der Spieler gestoppt wird
-    public float maxSpeed = 10f; // Die maximale Geschwindigkeit des Charakters
+    public float speed = 130f; // Die Geschwindigkeit des Spielers
 
-    private Rigidbody2D rb;
-    private Vector2 targetPosition;
-    private Vector2 lastMousePos;
-    private float lastDistance;
+    private Rigidbody2D rb; // Eine Referenz auf den Rigidbody2D des Spielers
 
+   
+
+
+    // Start wird einmal aufgerufen, wenn das Spiel startet
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.freezeRotation = true; // Deaktivieren der Rotation des Spielers
+        rb = GetComponent<Rigidbody2D>(); // Referenz auf den Rigidbody2D des Spielers holen
     }
 
-    void Update()
-    {
-        // Verfolgen Sie die Mausposition
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPosition = mousePos;
-
-        // Berechnen Sie die Entfernung und die Geschwindigkeit der Mausbewegung
-        float distance = Vector2.Distance(lastMousePos, mousePos);
-        float speedMultiplier = Mathf.Clamp(distance / lastDistance, 0f, 1f);
-
-        // Aktualisieren Sie die Geschwindigkeit des Charakters
-        speed = maxSpeed * speedMultiplier;
-
-        // Speichern Sie die aktuelle Mausposition und Entfernung
-        lastMousePos = mousePos;
-        lastDistance = distance;
-    }
-
+    // FixedUpdate wird in regelmäßigen Abständen aufgerufen, um Physik-Berechnungen durchzuführen
     void FixedUpdate()
     {
-        // Bewegen Sie den Spieler in Richtung der Mausposition
-        Vector2 movement = targetPosition - rb.position;
 
-        // Wenn die Entfernung kleiner als der Schwellenwert ist, wird der Spieler gestoppt
-        if (movement.magnitude < stopDistance)
-        {
-            rb.velocity = Vector2.zero;
-        }
-        else
-        {
-            rb.AddForce(movement.normalized * speed);
-        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        // Deaktivieren der Rotation des Spielers
-        rb.freezeRotation = true;
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        // Setzen Sie die maximale Geschwindigkeit des Charakters
+        float maxSpeed = 130f;
+
+        // Beschränken Sie die Geschwindigkeit des Charakters auf die maximale Geschwindigkeit
+        movement = Vector2.ClampMagnitude(movement, maxSpeed);
+
+        // Fügen Sie die Kraft hinzu, die den Charakter in die gewünschte Richtung bewegt
+        rb.AddForce(movement * speed);
+
+        rb.velocity *= 0.5f;
+       
     }
 }
