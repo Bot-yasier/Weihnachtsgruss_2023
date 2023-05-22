74aaacef54180604b833ac6807da923c602891ef
Bot-yasier <89403943+Bot-yasier@users.noreply.github.com>
2023-05-22 14:57:08 +0200

    Starting Random Room generation

---
+++ Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Room_", menuName = "Scriptable Objects/Dungeon/Room")]
+public class RoomTemplateSO : ScriptableObject
+{
+    [HideInInspector] public string guid;
+
+    [Space(10)]
+    [Header("ROOM PREFAB")]
+
+    [Tooltip("The gameobject prefap for the room(this will contain all the tilemaps for the room and environment game objects)")]
+
+
+    public GameObject prefab;
+
+    [HideInInspector] public GameObject previousPrefab;
+
+    [Space(10)]
+    [Header("ROOM CONFIGURATION")]
+
+
+    public RoomNodeTypeSO roomNodeType;
+
+    public Vector2Int lowerBounds;
+
+    public Vector2Int upperBounds;
+
+    [SerializeField] public List<Doorway> doorwayList;
+
+    public Vector2Int[] spawnPositionArray;
+
+    public List<Doorway> GetDoorwayList()
+    {
+        return doorwayList;
+
+    }
+
+    #region Validation
+
+#if UNITY_EDITOR
+
+    // Validate SO fields
+    private void OnValidate()
+    {
+        // Set unique GUID if empty or the prefab changes
+        if (guid == "" || previousPrefab != prefab)
+        {
+            guid = GUID.Generate().ToString();
+            previousPrefab = prefab;
+            EditorUtility.SetDirty(this);
+        }
+
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(doorwayList), doorwayList);
+
+        // Check spawn positions populated
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(spawnPositionArray), spawnPositionArray);
+#endif
+
+        #endregion Validation
+
+
+    }
+
+}
---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,9 @@
+
+public enum Orientation
+{
+   north,
+   easzt,
+   south,
+   west,
+   none
+}
