6806e32f7c5322745bb5877f3a877e0851f03702
Bot-yasier <89403943+Bot-yasier@users.noreply.github.com>
2023-08-28 08:35:53 +0200

    Game over update

--- Assets/Player/PlayerHealthController.cs
+++ Assets/Player/PlayerHealthController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerHealthController : MonoBehaviour
 {
+    public Animator animator;
+    public PlayerMovementMouse playerMovementMouse;
+    public Rigidbody2D playerrigid;
+    public GameObject PlayerSprite;
     public int startingHealth = 6;
     public int currentHealth;
     public GameObject Player;
@@ -27,7 +31,20 @@ public class PlayerHealthController : MonoBehaviour
         if (currentHealth == 3) { hard4.SetActive(false); hard5.SetActive(false); hard6.SetActive(false); hard1.SetActive(true); hard2.SetActive(true); hard3.SetActive(true); }
         if (currentHealth == 2) { hard3.SetActive(false); hard4.SetActive(false); hard5.SetActive(false); hard6.SetActive(false); hard1.SetActive(true); hard2.SetActive(true); }
         if (currentHealth == 1) { hard2.SetActive(false); hard3.SetActive(false); hard4.SetActive(false); hard5.SetActive(false); hard6.SetActive(false); hard1.SetActive(true); }
-        if (currentHealth == 0) { hard1.SetActive(false); hard2.SetActive(false); hard3.SetActive(false); hard4.SetActive(false); hard5.SetActive(false); hard6.SetActive(false); Menu.SetActive(true); Destroy(Player); }
+        if (currentHealth == 0) { hard1.SetActive(false); hard2.SetActive(false); hard3.SetActive(false); hard4.SetActive(false); hard5.SetActive(false); hard6.SetActive(false); Menu.SetActive(true);
+
+            animator.enabled = false;
+            playerMovementMouse.enabled = false;
+            playerrigid.constraints = RigidbodyConstraints2D.FreezePosition;
+            playerrigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+            GameObject[] enemyAmmoObjects = GameObject.FindGameObjectsWithTag("EnemyAmmo");
+            PlayerSprite.SetActive(false);
+
+            foreach (GameObject enemyAmmoObject in enemyAmmoObjects)
+            {
+                Destroy(enemyAmmoObject);
+            }
+        }
     }
 
     public void TakeDamage(int damageAmount)
