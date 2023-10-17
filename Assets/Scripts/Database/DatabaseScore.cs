2807c01d1272b25e0a1f4738d84b5008171faa87
Bot-yasier <89403943+Bot-yasier@users.noreply.github.com>
2023-10-17 09:50:42 +0200

    Database Score

---
+++ Assets/Scripts/Database/DatabaseScore.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class DatabaseScore : MonoBehaviour
+{
+    public PlayerHealthController playerHealth;
+    public string score; // Hier wird der Score gespeichert
+
+    private TextMeshProUGUI scoreTextMesh; // Hier wird das TextMeshProUGUI-Objekt gespeichert
+
+    private void Start()
+    {
+        // Finde das TextMeshProUGUI-Objekt mit dem Tag "Zahl"
+        scoreTextMesh = GameObject.FindGameObjectWithTag("Zahl").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        // Überprüfe, ob das GameObject mit dem Tag "Player" nicht mehr existiert
+        if (playerHealth.currentHealth == 0)
+        {
+            Debug.Log("Hallo");
+
+            score = scoreTextMesh.text;
+
+        }
+    }
+}
