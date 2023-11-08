c1c2989e080958805a2fd6a307ad10754c12fbb1
oilfoot <86773038+oilfoot@users.noreply.github.com>
2023-11-08 14:52:54 +0100

    ScoreBoard V2

--- Assets/Networking/ScoreReceiver.cs
+++ Assets/Networking/ScoreReceiver.cs
@@ -5,13 +5,12 @@ using TMPro;
 
 public class ScoreReceiver : MonoBehaviour
 {
-    // Docker
-    //private const string backendUrl = "/api/score/HandleGetRequest"; // Replace with your actual backend GET URL
-
     // Testing
     private const string backendUrl = "http://localhost:9001/score/HandleGetRequest";
 
-    public TMP_Text scoreText;
+    public TMP_Text textBox1;
+    public TMP_Text textBox2;
+    public TMP_Text textBox3;
 
     // Start is called before the first frame update
     void Start()
@@ -36,27 +35,41 @@ public class ScoreReceiver : MonoBehaviour
             // Log the error if the request fails
             Debug.LogError($"GET request failed: {www.error}");
 
-            // Display "error" in TMP text component
-            scoreText.text = "Error fetching data";
+            // Display "error" in TMP text components
+            textBox1.text = "Error fetching data";
+            textBox2.text = "";
+            textBox3.text = "";
         }
         else
         {
             // Parse the received JSON data
             string responseText = www.downloadHandler.text;
 
-            // Split the response by newline characters
-            string[] lines = responseText.Split('\n');
+            // Split the response by semicolon characters
+            string[] lines = responseText.Split(';');
 
-            // Clear the existing text in the TMP text component
-            scoreText.text = "";
+            // Clear the existing text in the TMP text components
+            textBox1.text = "";
+            textBox2.text = "";
+            textBox3.text = "";
 
-            // Iterate through the lines and display them
+            // Iterate through the lines and display them in the corresponding text boxes
             foreach (string line in lines)
             {
                 string trimmedLine = line.Trim();
                 if (!string.IsNullOrEmpty(trimmedLine))
                 {
-                    scoreText.text += trimmedLine + "\n";
+                    // Split each line by the comma (",") character
+                    string[] parts = trimmedLine.Split(',');
+
+                    // Ensure there are at least three parts (comma-separated values) in the line
+                    if (parts.Length >= 3)
+                    {
+                        // Assign the parts to the text boxes
+                        textBox1.text += parts[0] + "\n";
+                        textBox2.text += parts[1] + "\n";
+                        textBox3.text += parts[2] + "\n";
+                    }
                 }
             }
         }
