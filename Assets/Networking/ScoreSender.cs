526d3f62897891e072205d4cc4cd215e315af992
oilfoot <86773038+oilfoot@users.noreply.github.com>
2023-11-22 09:55:56 +0100

    Security Update

--- Assets/Networking/ScoreSender.cs
+++ Assets/Networking/ScoreSender.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 using UnityEngine.Networking;
 using TMPro;
 using UnityEngine.UI;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System;
 
 public class ScoreSender : MonoBehaviour
 {
     private const string backendUrl = "/api/score/HandlePostRequest"; // Replace with your actual backend URL
+
+    //private const string backendUrl = "http://localhost:9001/score/HandlePostRequest";
+
+    //private const string backendUrl = "https://localhost:32768/score/HandlePostRequest";
+
     public DatabaseScore databasescore;
+
+    public string key;
 
     public Button SendScoretoD;
 
@@ -56,9 +67,30 @@ public class ScoreSender : MonoBehaviour
         // Create a JSON string with the score, escaped playerName, and escaped emailAddress
         string jsonPayload = $"{{ \"Score\": {score}, \"PlayerName\": \"{escapedPlayerName}\", \"Email\": \"{escapedEmailAddress}\" }}";
 
+        // Encrypt JSON-String
+        string jsonEncrypted;
+        Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = new byte[16];
+        ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                {
+                    streamWriter.Write(jsonPayload);
+                }
+                jsonEncrypted = Convert.ToBase64String(memoryStream.ToArray());
+            }
+        }
+        //Debug.Log("Key: " + key);
+        //Debug.Log("Plain JSON: " + jsonPayload);
+        Debug.Log("Encrypted JSON: " + jsonEncrypted);
+
         // Create a UnityWebRequest to send the POST request
         UnityWebRequest www = new UnityWebRequest(backendUrl, "POST");
-        byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonPayload);
+        byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonEncrypted);
         www.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonBytes);
         www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
         www.SetRequestHeader("Content-Type", "application/json");
