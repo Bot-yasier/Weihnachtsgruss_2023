1352ef6baa4052b8b1ba9b0e3cd188cb228fd839
oilfoot <86773038+oilfoot@users.noreply.github.com>
2023-12-08 11:13:04 +0100

    FPS Handler

--- Assets/Scripts/FPS/FPSHandler.cs
+++ Assets/Scripts/FPS/FPSHandler.cs
@@ -1,32 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FPSHandler : MonoBehaviour
 {
 
     public GameObject FPSobj;
-    public float targetFramerate = 30f;
+    public float targetFramerate = 15f;
+    public Button Shut;
+    bool Start1 = false;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shut.onClick.AddListener(Shut1);
+        StartCoroutine(wait());
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        float currentFramerate = 1.0f / Time.deltaTime;
-
-        if(currentFramerate < targetFramerate)
+        if(Start1 == true)
         {
-            FPSobj.SetActive(true);
+            float currentFramerate = 1.0f / Time.deltaTime;
 
+            if (currentFramerate < targetFramerate)
+            {
+                FPSobj.SetActive(true);
+
+            }
         }
-        else
-        {
-            FPSobj.SetActive(false);
-        }
+
+
+    }
+
+    void Shut1()
+    {
+        FPSobj.SetActive(false);
+    }
+
+    private IEnumerator wait()
+    {
+        yield return new WaitForSeconds(5f);
+        Start1 = true;
+
     }
 }
